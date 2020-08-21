737f14e5bd0616415edc68016ebb3136cf9ddefb
Noronha <inc.691@gmail.com>
2020-08-21 16:42:27 -0300

    Add number of notification baloon on each button which can handle any kind of notification

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLAttributes, ButtonHTMLAttributes, useState } from 'react';
+import React, { HTMLAttributes, useState } from 'react';
 import { MdSearch, MdGames } from 'react-icons/md';
 import { FiYoutube } from 'react-icons/fi';
 import { 
@@ -19,11 +19,23 @@ import './styles.css';
 interface NavButtonProps extends HTMLAttributes<HTMLButtonElement> {
   Icon: IconType;
   subtitle: string;
+  notification?: number;
 }
 
-const NavButton: React.FC<NavButtonProps> = ({ Icon, subtitle, ...buttonProps }) => {
+const NavButton: React.FC<NavButtonProps> = ({ Icon, subtitle, notification = 0,...buttonProps }) => {
   return (
     <button {...buttonProps} title={subtitle} >
+      {notification !== 0 && <span className="notification">{notification}</span>}
+      <Icon />
+    </button>
+  );
+}
+
+const MenuButton: React.FC<NavButtonProps> = ({ Icon, subtitle, notification = 0, ...buttonProps }) => {
+
+  return (
+    <button {...buttonProps} title={subtitle}>
+      {notification !== 0 && <span className="notification">{notification}</span>}
       <Icon />
     </button>
   );
@@ -52,10 +64,10 @@ const Header: React.FC = () => {
         
         <section className="page-nav">
           <NavButton onClick={() => setActivePage(1)} className={activePage === 1 ? "nav-page active" : "nav-page" } Icon={FaHome} subtitle="Página Inicial" />
-          <NavButton onClick={() => setActivePage(2)} className={activePage === 2 ? "nav-page active" : "nav-page" } Icon={FiYoutube} subtitle="Watch" />
+          <NavButton notification={2} onClick={() => setActivePage(2)} className={activePage === 2 ? "nav-page active" : "nav-page" } Icon={FiYoutube} subtitle="Watch" />
           <NavButton onClick={() => setActivePage(3)} className={activePage === 3 ? "nav-page active" : "nav-page" } Icon={FaStore} subtitle="Marketplace" />
           <NavButton onClick={() => setActivePage(4)} className={activePage === 4 ? "nav-page active" : "nav-page" } Icon={FaUsers} subtitle="Grupos" />
-          <NavButton onClick={() => setActivePage(5)} className={activePage === 5 ? "nav-page active" : "nav-page" } Icon={MdGames} subtitle="Jogos" />
+          <NavButton notification={5} onClick={() => setActivePage(5)} className={activePage === 5 ? "nav-page active" : "nav-page" } Icon={MdGames} subtitle="Jogos" />
         </section>
 
         <section className="menu-nav">
@@ -63,18 +75,11 @@ const Header: React.FC = () => {
             <img src={avatar} alt="user-avatar"/>
             <span className="user-name">Gabriel Noronha</span>
           </div>
-          <button onClick={() => setActiveMenu(1)} className={activeMenu === 1 ? "nav-menu active" : "nav-menu" }>
-            <FaPlus />
-          </button>
-          <button onClick={() => setActiveMenu(2)} className={activeMenu === 2 ? "nav-menu active" : "nav-menu" }>
-            <FaFacebookMessenger />
-          </button>
-          <button onClick={() => setActiveMenu(3)} className={activeMenu === 3 ? "nav-menu active" : "nav-menu" }>
-            <FaBell />
-          </button>
-          <button onClick={() => setActiveMenu(4)} className={activeMenu === 4 ? "nav-menu active" : "nav-menu" }>
-            <FaSortDown />
-          </button>
+
+          <MenuButton Icon={FaPlus} subtitle="Criar" onClick={() => setActiveMenu(1)} className={activeMenu === 1 ? "nav-menu active" : "nav-menu" }/>
+          <MenuButton Icon={FaFacebookMessenger} notification={4} subtitle="Messenger" onClick={() => setActiveMenu(2)} className={activeMenu === 2 ? "nav-menu active" : "nav-menu" }/>
+          <MenuButton Icon={FaBell} notification={1} subtitle="Notificações" onClick={() => setActiveMenu(3)} className={activeMenu === 3 ? "nav-menu active" : "nav-menu" }/>
+          <MenuButton Icon={FaSortDown} subtitle="Conta" onClick={() => setActiveMenu(4)} className={activeMenu === 4 ? "nav-menu active" : "nav-menu" }/>
         </section>
     </header>
   );
